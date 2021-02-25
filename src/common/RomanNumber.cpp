7e7412923d966fbc900601d6c873357148fb5bc9
ravielze <ravielz20@gmail.com>
2021-02-25 15:19:07 +0700

    roman number constant

--- src/common/RomanNumber.cpp
+++ src/common/RomanNumber.cpp
@@ -1,8 +1,8 @@
 #include "RomanNumber.hpp"
 
-int num[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
-string sym[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
-int MAX_INDEX = 12;
+const int num[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+const string sym[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+const int MAX_INDEX = 12;
 
 string getRoman(int number)
 {
