d3d93dda1fb7931954dae2cbbe0ce826b1d70cf3
ravielze <ravielz20@gmail.com>
2021-02-27 20:51:18 +0700

    Remove unused library

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include "graph/graph.hpp"
 #include "reader/reader.hpp"
 #include <chrono>
-#include <ctime>
-#include <ratio>
 using namespace std;
 
 int main(int argc, char **argv)
@@ -16,13 +14,14 @@ int main(int argc, char **argv)
     chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
     Reader r(argv[1]);
     Graph g(r.getLines());
-    if (argc >= 3){
+    if (argc >= 3)
+    {
         int a = atoi(argv[2]);
         g.setTab(a);
     }
     g.topologicalSort();
     chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
     chrono::duration<double, milli> time_span = t2 - t1;
-    cout << "Waktu yang dibutuhkan: " << time_span.count()/1000 << " detik." << endl;
+    cout << "Waktu yang dibutuhkan: " << time_span.count() / 1000 << " detik." << endl;
     return 0;
 }
